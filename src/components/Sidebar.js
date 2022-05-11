f032f1c51c6061417953df46f5337c2c11939d95
JaneMoroz <moroz.jane@yahoo.com>
2022-05-11 18:20:46 +0300

    Added Sidebar Functionality,  About/Error/Hero (partially) Pages

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,13 +9,13 @@ import CartButtons from "./CartButtons";
 import { useUserContext } from "../context/user_context";
 
 const Sidebar = () => {
-  const isOpen = false;
+  const { isSidebarOpen, closeSidebar } = useProductsContext();
   return (
     <SidebarContainer>
-      <aside className={`sidebar ${isOpen ? "show-sidebar" : ""}`}>
+      <aside className={`sidebar ${isSidebarOpen ? "show-sidebar" : ""}`}>
         <div className="sidebar-header">
           <img src={logo} alt="Comfy Sloath" />
-          <button className="close-btn" type="button">
+          <button className="close-btn" type="button" onClick={closeSidebar}>
             <FaTimes />
           </button>
         </div>
@@ -23,12 +23,16 @@ const Sidebar = () => {
           {links.map(({ id, text, url }) => {
             return (
               <li key={id}>
-                <Link to={url}>{text}</Link>
+                <Link to={url} onClick={closeSidebar}>
+                  {text}
+                </Link>
               </li>
             );
           })}
           <li>
-            <Link to="/checkout">Checkout</Link>
+            <Link to="/checkout" onClick={closeSidebar}>
+              Checkout
+            </Link>
           </li>
         </ul>
         <CartButtons />
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem
+            consectetur tempora nihil minus maiores deserunt impedit
+            voluptatibus ad quod magnam? Autem, doloremque molestiae dolorem ea
+            cupiditate quis incidunt magnam earum! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Aliquid laborum in quasi officiis ipsa
+            non asperiores corrupti cumque incidunt consequatur.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +54,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
